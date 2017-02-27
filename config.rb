560335aaa0195437f12494b01471d97380a016a9
Bill Domanick <bill.domanick@crowdsurge.com>
2017-02-27 00:25:50 -0500

    main build more to come

---
+++ config.rb
@@ -0,0 +1,10 @@
+# default commands found at: http://compass-style.org/help/documentation/configuration-reference/
+# Set this to the root of your project when deployed:
+css_dir = '_/css'
+sass_dir = '_/build/sass'
+javascripts_dir = '_/js'
+images_dir = "_/images"
+line_comments = false
+
+# One of: :nested, :expanded, :compact, or :compressed.
+output_style = :compact
